ae1631e65f8f5b071e6daa6eb8b555812eebaef2
chakresh kolluru <chakresh.kolluru@infostretch.com>
2018-10-11 12:07:14 +0530

    Modified Groovy file

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,4 +1,21 @@
-def call(String name){
+def call(){
+	node{
+		stage 'checkout'
+		git "https://github.com/chakreshkolluru/Myrepository.git"
 
-	echo "Hello  ${name}"
+		dir('sample'){
+			stage 'compile'
+			sh "javac Hello.java"
+
+			stage 'Run'
+			sh "java Hello"
+		}
+
+		post{
+			success{
+				echo "----Library is Successfully Imported----"
+				echo "Build is success"
+			}
+		}
+	}
 }
