6fd64364fa3b59a810718d22d23c4b56951a9c0c
chakresh kolluru <chakresh.kolluru@infostretch.com>
2018-10-15 15:17:19 +0530

    Added Src Folder

--- src/Example.groovy
+++ src/Example.groovy
@@ -2,7 +2,7 @@ class Example{
 	String s;
     Example(String str){this.s=str;}
 
-    void method1()
+    def method1()
     {
     	echo "This is a groovy class file with a string parameter   ${s}"
     }
