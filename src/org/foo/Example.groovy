b0709abeccb8401ab162558185c1baf07e93673b
chakreshkolluru <42858947+chakreshkolluru@users.noreply.github.com>
2018-11-16 15:19:44 +0530

    Update Example.groovy

--- src/org/foo/Example.groovy
+++ src/org/foo/Example.groovy
@@ -2,11 +2,11 @@ package org.foo;
 
 class Example{
 	String s;
-    Example(String str){this.s=str;}
+    Example(String s){this.s=s;}
 
     def method1()
     {
-    	print "This is a groovy class file with a string parameter   ${s}"
+    	print ("This is a groovy class file with a string parameter   ${s}")
     }
 
 }
